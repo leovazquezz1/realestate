0abb0322d597c809442ceefdb8f0b0b7d8eaa19f
Leandro Rios <leandrorv26@gmail.com>
2021-01-20 01:39:04 -0400

    minor

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ var options = {
 
 mongoose.Promise = require('bluebird');
 
-mongoose.connect(encodeURI(process.env.MONGOHQ_URL), options);
+mongoose.connect(encodeURI(process.env.MONGOHQ_URL), { useNewUrlParser: true });
 
 var db = mongoose.connection;
 
