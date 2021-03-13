005536b369adf50a65b0ae92818ac084d68a9d3f
Leandro Rios <leandrorv26@gmail.com>
2021-03-13 17:52:48 -0400

    added expireAfterSeconds to post schema

--- models/Post.js
+++ models/Post.js
@@ -3,16 +3,21 @@ mongoose.Promise = global.Promise;
 const Schema = mongoose.Schema;
 const dataTables = require('mongoose-datatables');
 
+const secondInADay = 86400;
+
 var PostSchema = new Schema({
     id: {type: String, unique: true},
     url: String,
     price: Number,
     description: String,
-    image: String
+    image: String,
+    phone: String
 }, {
     timestamps: true
 });
 
 PostSchema.plugin(dataTables);
+
+mongoose.connection.collection("posts").createIndex( { "createdAt": 1 }, { expireAfterSeconds: (secondInADay * 30) } )
 
 module.exports = mongoose.model('Post', PostSchema);
